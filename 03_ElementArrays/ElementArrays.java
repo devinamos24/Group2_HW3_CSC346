2f2432a554b28b94458ece5613710d7729472fc2
damos2 <damos2@missouriwestern.edu>
2022-03-01 08:08:28 -0600

    feat: add ElementArrays code example with comments

--- 03_ElementArrays/ElementArrays.java
+++ 03_ElementArrays/ElementArrays.java
@@ -1,5 +1,6 @@
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -19,26 +20,34 @@ public class ElementArrays {
 
             File xmlFile = new File("03_ElementArrays/ElementArrays.xml");
             Document document = builder.parse(xmlFile);
-            document.getDocumentElement().normalize();
 
             // TODO: add code for parsing array xml document
 
             // First we need to grab the node that holds the array
             // In this example that happens to be the root node
             Element root = document.getDocumentElement();
+            root.normalize();
 
-            // After we obtain the Element containing the array, we need to pull the array
-            NodeList element_array_raw = root.getElementsByTagName("state");
+            // After we obtain the Element containing the array, we need to pull the first child in the array
+            // This allows us to parse each sibling after the child like a Linked List
+            Node node = root.getFirstChild();
 
-            for (int i = 0; i < element_array_raw.getLength(); i++)
-                System.out.println(element_array_raw.item(i).getChildNodes().getLength());
             // Next we need to parse the array and get the data we want
             // Point out that for each loops are not available for NodeList objects
-//            Element currentitem;
-//            for (int i = 0; i < element_array.getLength(); i++) {
-//                currentitem = (Element) element_array.item(i);
-//                System.out.println(currentitem.getElementsByTagName("state").item(0).getTextContent());
-//            }
+            // We use a do while loop so that we include the first element we selected before
+            // We also must check that the node name matches the name we want, in this case it is "state"
+            // There are weird newline nodes between each element, the if statement helps ignore them
+            do {
+                if (node.getNodeName().equals("state")) {
+                    System.out.println(((Element) node).getElementsByTagName("state").item(0).getTextContent());
+                    System.out.println(((Element) node).getElementsByTagName("slug").item(0).getTextContent());
+                    System.out.println(((Element) node).getElementsByTagName("code").item(0).getTextContent());
+                    // I'm too lazy to add the rest, but you get the point
+                }
+            } while ((node = node.getNextSibling()) != null);
+
+            // Be careful with dom nodes because they don't always have an important meaning
+            // For instance, there are nodes between each element that are just there for the newline character
 
         } catch (ParserConfigurationException | IOException | SAXException e) {
             System.out.println(e.getMessage());
